6a61f549d3414de8f9ff73350d2c81995936fc6a
Satyajit Samal <samalsatyajit668@gmail.com>
2024-10-23 20:14:13 +0530

    Created the functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import './App.css'
-
+import { useState } from 'react';
+import './App.css';
 
 function App() {
 
+  const [color, setColor] = useState('')
+
+  const handleOnChange = (e) => {
+    setColor(e.target.value)
+  }
+``
   return (
+
     <>
       <input 
       type="text" 
-
+      className='w-[500px] h-[200px] m-5 p-10'
+      readOnly
+      placeholder='Color of the input box'
+      style={{backgroundColor: `${color}`}}
       />
-
+      <br />
       <input 
       type="text" 
-      
+      className='w-200 h-5 p-1'
+      placeholder='Input Colour'
+      onChange={handleOnChange}
       />
     </>
   )
